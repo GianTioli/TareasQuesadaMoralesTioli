45bee90cc659b54973d818fa49a2eadb5c74a61b
GianTioli <gtioliantillon@gmail.com>
2025-03-02 15:32:57 -0600

    Commit 4: Agrega un nuevo archivo con errores de flake8

---
+++ Tarea1/errores_flake8.py
@@ -0,0 +1,11 @@
+# Los errores esperados son:
+# E302 expected 2 blank lines, found 1
+# E117 over-indented
+# E211 whitespace before '('
+
+def funcion_1():
+    x = 1+2
+    return x
+
+def Funciondos():
+        print ("hola")
